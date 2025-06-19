e51a4ebde16f308bae34e6d427e1474e2aa854ad
pauchok-sk <pauchoooooook@gmail.com>
2025-06-19 13:36:04 +0300

    s-contacts

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 import "../scss/style.scss";
 import burger from "./files/burger.js";
+import map from "./files/map.js";
+import mediaAdaptive from "./files/mediaAdaptive.js";
 import sliders from "./files/sliders.js";
 import spoller from "./files/spoller.js";
 
 spoller();
 burger();
 sliders();
+map();
+mediaAdaptive();
 
-Fancybox.bind("[data-fancybox]")
+Fancybox.bind("[data-fancybox]");
